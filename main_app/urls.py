29beafcc6a304a4853bd030be5858c2829ffd709
boyoon-c <boyoonchang@gmail.com>
2021-08-30 22:00:16 -0700

    Stub up index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name="about"),
+    path('puppies/', views.puppies_index, name="puppies_index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,35 @@
+"""
+from django.http import HttpResponse
+
+Create your views here.
+def home(request):
+    return HttpResponse('<h1>HOME</h1>')
+
+def about(request):
+    return HttpResponse('<h1>ABOUT</h1>')
+"""
+
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-# Create your views here.
-# def home(request):
-#     return HttpResponse('<h1>HOME</h1>')
-
-# def about(request):
-#     return HttpResponse('<h1>ABOUT</h1>')
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+class Puppies:  
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+puppies = [
+  Puppies('Charles', 'Golden Retriever', 'Very playful.', 1),
+  Puppies('Bob', 'Golden Retriever', 'Very cute', 0),
+  Puppies('Winky', 'Golden Retriever', 'Very energetic', 4),
+  Puppies('Rocky', 'Golden Retriever', 'Loves strangers', 6)
+]
+
+def puppies_index(request):
+  return render(request, 'puppies/index.html', { 'puppies': puppies })
