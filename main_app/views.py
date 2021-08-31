c7a57c234fa1aaaff99cb2065e3494b0002c21cb
boyoon-c <boyoonchang@gmail.com>
2021-08-30 21:37:47 -0700

    Stub up about page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
+# def home(request):
+#     return HttpResponse('<h1>HOME</h1>')
+
+# def about(request):
+#     return HttpResponse('<h1>ABOUT</h1>')
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
